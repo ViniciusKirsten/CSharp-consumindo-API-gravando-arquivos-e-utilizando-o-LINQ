56aff64e854a843c509968f053d2aa532b7f37d2
Vinicius Mendonca <vinicius.k.mendonca@outlook.com>
2025-02-25 22:28:31 -0300

    Inicio do curso

---
+++ ScreenSound/ScreenSound/Modelos/Musica.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ScreenSound.Modelos
+{
+    internal class Musica
+    {
+        //para que ele possa pegar dentro da minha API a informação do campo que apontamos, neste caso (song)
+        [JsonPropertyName("song")] //esse tributo é conhecido como anotação
+        public string? Nome { get; set; }
+
+        [JsonPropertyName("artist")]
+        public string? Artista { get; set; }
+
+    }
+}
---
+++ ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,15 @@
+﻿//requisição da nossa API - HttpClient é um tipo já criado dentro do C#
+using (HttpClient client = new HttpClient())
+{
+	try
+	{
+        //await - para ele esperar a tarefa ser concluida
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+    }
+	catch (Exception ex)
+	{
+		Console.WriteLine($"Temos um problema: {ex.Message}");
+	}
+}
+//vamos utilizar esse pacote apenas dentro desse pacote
